35afdd60f249cc91fed82078929c2a3f35c81c74
takuya <takuya_916@me.com>
2026-04-12 01:17:20 +0900

    マウス修正、HEモード不能問題あり

--- keyboards/keychron/q1_he/ansi_encoder/keymaps/my_map/keymap.c
+++ keyboards/keychron/q1_he/ansi_encoder/keymaps/my_map/keymap.c
@@ -235,11 +235,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //-------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
         //-------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+
-        KC_TAB,   KC_Q,     XXXXXXX,  KC_E,     KC_R,     KC_T,     KC_Y,     KC_WH_U,  KC_I,     KC_WH_D,  KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
+        KC_TAB,   KC_Q,     XXXXXXX,  KC_E,     KC_R,     KC_T,     KC_Y,     MS_WHLU,  KC_I,     MS_WHLD,  KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
         //-------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+
-        KC_CAPS,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_F,     KC_G,     KC_BTN4,  KC_BTN1,  KC_BTN2,  KC_L,     XXXXXXX,  KC_QUOT,            KC_ENT,             KC_HOME,
+        KC_CAPS,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_F,     KC_G,     MS_BTN4,  MS_BTN1,  MS_BTN2,  KC_L,     XXXXXXX,  KC_QUOT,            KC_ENT,             KC_HOME,
         //-------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_BTN5,  KC_BTN3,  KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     MS_BTN5,  MS_BTN3,  KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,
         //-------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 FN_1,     FN_2,     KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
     ),
@@ -332,10 +332,15 @@ void matrix_scan_user(void) {
         int8_t move_y = 0;
         uint8_t deadzone = 0;
 
-        if (val_w > deadzone) move_y -= (uint16_t)val_w * val_w / 230;
-        if (val_s > deadzone) move_y += (uint16_t)val_s * val_s / 230;
-        if (val_a > deadzone) move_x -= (uint16_t)val_a * val_a / 230;
-        if (val_d > deadzone) move_x += (uint16_t)val_d * val_d / 230;
+        uint8_t travel_w = val_w / 6;
+        uint8_t travel_a = val_a / 6;
+        uint8_t travel_s = val_s / 6;
+        uint8_t travel_d = val_d / 6;
+
+        if (travel_w > deadzone) move_y -= (uint16_t)travel_w * travel_w / 230;
+        if (travel_s > deadzone) move_y += (uint16_t)travel_s * travel_s / 230;
+        if (travel_a > deadzone) move_x -= (uint16_t)travel_a * travel_a / 230;
+        if (travel_d > deadzone) move_x += (uint16_t)travel_d * travel_d / 230;
 
         if (move_x != 0 || move_y != 0) {
             report_mouse_t mouse_report = {0};
